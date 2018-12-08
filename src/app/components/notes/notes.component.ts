c9d3e7a749b764e50b40c483aac53531c5cf6396
oztek22 <yashndave@gmail.com>
2018-12-08 17:49:35 +0530

    remove notes

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -31,7 +31,22 @@ export class NotesComponent implements OnInit {
   }
 
   changeEditMode(value) {
+    this.notes = this.notes.map((e) => {
+      e.isRemove = false;
+      return e;
+    });
+    this.dataService.updateNote(this.notes);
     this.editMode = value;
+  }
+
+  removeNotes() {
+    console.log(this.notes);
+    this.notes.forEach((element, index) => {
+      if (element.isRemove) {
+        this.notes.splice(index, 1);
+      }
+    });
+    this.dataService.updateNote(this.notes);
   }
 
 }
